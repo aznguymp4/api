3f853cf0ab377a14e51c5df1cda41d6c53b47354
aznguy.mp4 <48527495+aznguymp4@users.noreply.github.com>
2022-02-04 22:21:26 -0600

    asdasdads

--- bot.js
+++ bot.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.LOCALPORT||process.env.PORT
 const IP = process.env.IP
+const fetch = require('node-fetch')
 const request = require('request')
+process.on('uncaughtException', function(err) { console.error(err?.stack||err) })
 
 app.get('/*', function(req,res) {
 	let link = `https://${IP}:${PORT}${res.socket.parser.incoming.url}`
@@ -12,3 +14,5 @@ app.get('/*', function(req,res) {
 app.listen(PORT, ()=>{
     console.log(`API running on ${IP}:${PORT}`)
 })
+setInterval(()=>{ fetch('http://api.aznguy.com',{method:'GET'}) },1500000)
+// ping every 25 minutes so it doesn't go to sleep mode cause heroku big dumb
