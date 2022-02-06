0cbc208bf64d3ae63a743d24c127bba0a989a4e4
aznguy.mp4 <48527495+aznguymp4@users.noreply.github.com>
2022-02-06 01:30:14 -0600

    ads

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import fetch from 'node-fetch'
 import request from 'request'
-import https from 'https'
+//import https from 'https'
 dotenv.config({path:'./.env'})
 const app = express()
 const PORT = process.env.LOCALPORT||process.env.PORT
@@ -15,12 +15,14 @@ const httpsOptions = {
 console.log(httpsOptions)
 
 app.get('/*', function(req,res) {
-	res.status(200).send('HELLO')
-	//let link = `http://${IP}:${PORT}${res.socket.parser.incoming.url}`
-	//request(link).pipe(res)
+	//res.status(200).send('HELLO')
+	let link = `http://${IP}:${PORT}${res.socket.parser.incoming.url}`
+	request(link).pipe(res)
 })
 
-https.createServer(httpsOptions,app).listen(process.env.PORT,()=>{
+//https.createServer(httpsOptions,app)
+app
+.listen(process.env.PORT,()=>{
     console.log(`API running on ${IP}:${PORT}`)
 })
 setInterval(()=>{ fetch('https://api.aznguy.com',{method:'GET'}) },1500000)
