e5b7f5d3fd9234a9ccb9e69e10b0a17c7e5d08e2
aznguy.mp4 <48527495+aznguymp4@users.noreply.github.com>
2022-02-06 00:29:55 -0600

    da

--- bot.js
+++ bot.js
@@ -1,18 +1,25 @@
+process.on('uncaughtException', function(err) { console.error(err?.stack||err) })
 import dotenv from 'dotenv'
 import express from 'express'
 import fetch from 'node-fetch'
 import request from 'request'
+import https from 'https'
 dotenv.config({path:'./.env'})
 const app = express()
 const PORT = process.env.LOCALPORT||process.env.PORT
 const IP = process.env.IP
-process.on('uncaughtException', function(err) { console.error(err?.stack||err) })
+const httpsOptions = {
+	cert : process.env.CERT,
+	key  : process.env.KEY
+}
+console.log(httpsOptions)
 
 app.get('/*', function(req,res) {
 	let link = `http://${IP}:${PORT}${res.socket.parser.incoming.url}`
 	request(link).pipe(res)
-});
-app.listen(process.env.PORT, ()=>{
+})
+
+https.createServer(httpsOptions,app).listen(process.env.PORT,()=>{
     console.log(`API running on ${IP}:${PORT}`)
 })
 setInterval(()=>{ fetch('http://api.aznguy.com',{method:'GET'}) },1500000)
